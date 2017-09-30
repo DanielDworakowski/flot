aa5f9701cdaab93ef9914b52d2eaf6888ac516c8
Daniel <dworakowskidaniel@gmail.com>
2017-09-30 00:12:29 -0400

    setup: update to necessary paths

--- setup.sh
+++ setup.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
-
-echo export PYTHONPATH=$PWD/enivornment:\$PYTHONPATH >> $HOME/.bashrc
+#
+# Setup paths.
+echo export PYTHONPATH=$PWD/util:$PWD/agents:$PWD/configs:$PWD/environment:$PWD/external/AirSim/PythonClient:\$PYTHONPATH >> $HOME/.bashrc
+export LOCAL_HOME=$PWD
+source $HOME/.bashrc
+#
+# Initialize the airsim sub-module.
+git submodule update --init --recursive
+#
+# Initialize airsim.
+cd external/AirSim
+./setup.sh
+./build.sh
+cd $LOCAL_HOME
+#
+# Install the rate limiter python package.
+pip install ratelimiter
