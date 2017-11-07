fb1a5a62cdc1c6109506a6054fd6cd1f14313d64
Rae <raychanjeong@gmail.com>
2017-11-06 20:20:02 -0500

    add ros rate for image publishing

--- rasp/blimp_ws/src/blimp_control/src/ROSPiCam.py
+++ rasp/blimp_ws/src/blimp_control/src/ROSPiCam.py
@@ -17,34 +17,39 @@ import rospy
 from sensor_msgs.msg import CompressedImage
 
 
-rospy.init_node('ROSPiCam')
 
 # Init camera and output RGB array
 camera = picamera.PiCamera()
 
-image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size = 1)
 
-# Set publisher node to 15 Hz
-rate = rospy.Rate(15)
+def main():
+    image_pub = rospy.Publisher("/output/image_raw/compressed", CompressedImage, queue_size = 1)
+    rospy.init_node('ROSPiCam')
+    rate = rospy.Rate(10) # 10hz
+    # Set publisher node to 15 Hz
+    msg = CompressedImage()
+    msg.format = "jpeg"
+    # Publisher node loop
+    while not rospy.is_shutdown():
+        # Open new BytesIO stream
+        stream = io.BytesIO()
 
-msg = CompressedImage()
-msg.format = "jpeg"
+        # Capture frame from camera
+        camera.capture(stream, format='jpeg')
+        # camera.capture("/home/pi/temp.jpg")
 
-# Publisher node loop
-while not rospy.is_shutdown():
-    # Open new BytesIO stream
-    stream = io.BytesIO()
+        msg.header.stamp = rospy.Time.now()
+        msg.data = np.array(Image.open(stream)).tostring()
 
-    # Capture frame from camera
-    camera.capture(stream, format='jpeg')
-    # camera.capture("/home/pi/temp.jpg")
+        # Publish image
+        image_pub.publish()
 
-    msg.header.stamp = rospy.Time.now()
-    msg.data = np.array(Image.open(stream)).tostring()
+        # Close stream
+        stream.close()
+        rate.sleep()
 
-    # Publish image
-    image_pub.publish()
-
-    # Close stream
-    stream.close()
-
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
