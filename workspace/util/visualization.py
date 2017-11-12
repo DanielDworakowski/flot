21c1d2ba2f804f502fdfe3c8a99ec8fda027f972
Daniel <dworakowskidaniel@gmail.com>
2017-11-11 22:11:17 -0500

    clean up visualization a bit

--- workspace/util/visualization.py
+++ workspace/util/visualization.py
@@ -5,8 +5,35 @@ import torch
 from DataUtil import plotSample
 from tensorboardX import SummaryWriter
 from torch.autograd import Variable
+import argparse
+#
+# Parse the input arguments.
+def getInputArgs():
+    parser = argparse.ArgumentParser('Create visualization based on a network.')
+    parser.add_argument('--config', dest='configStr', default='DefaultNNConfig', type=str, help='Name of the config file to import.')
+    args = parser.parse_args()
+    return args
+#
+# Get the configuration, override as needed.
+def getConfig(args):
+    configuration = __import__(args.configStr)
+    conf = configuration.Config()
+    #
+    # Modifications to the configuration happen here.
+    conf.useTensorBoard = args.useTensorBoard
+    return conf
+#
+# Gather data.
+def gatherResponses(args):
 
+
+
+#
+# main.
 if __name__ == '__main__':
+    args = getInputArgs()
+    conf - getConfig(args)
+    gatherResponses(conf)
     #
     # The default configuration.
     conf = DefaultNNConfig.Config()
@@ -18,13 +45,14 @@ if __name__ == '__main__':
     labelsStack = None
     softmax_prob = torch.nn.Softmax()
     for data in dataset:
-        writer.add_image('Image', data['img'], 0)
-        writer.add_text('Text', 'text logged at step:'+str(1), 1)
-        # plotSample(data)
-        print("hi genie")
-        out = conf.hyperparam.model(Variable(data['img']))
+        out = None
+        if self.conf.usegpu:
+            out = conf.hyperparam.model(Variable(data['img']).cuda(async = True))
+        else:
+            out = conf.hyperparam.model(Variable(data['img']))
+
         labels = data['labels']
-        if not probability: 
+        if not probability:
             probability = softmax_prob(out).data.cpu()
             labelsStack = data['labels']
         else:
