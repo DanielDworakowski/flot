e34c58aeba64946da65daf27440cc514a7bbb06e
raejeong <raychanjeong@gmail.com>
2017-11-26 18:14:28 -0500

    raeconfig

---
+++ workspace/nn/config/RaeNNConfig.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from torchvision import transforms, models
+import pathlib
+import torch
+import torch.nn as nn
+import DataUtil
+import os
+from DefaultNNConfig import DefaultConfig
+#
+# Class to use the default configuration.
+class Config(DefaultConfig):
+    #
+    # Initialize.
+    def __init__(self):
+        super(Config, self).__init__()
+        self.hyperparam.numEpochs = 32
+        self.epochSaveInterval = 1
+
+        self.modelSavePath = '/home/user/workspace/data/model/'
+        self.modelLoadPath = '/home/user/workspace/data/model/model_best.pth.tar'
+        super(Config, self).loadModel()
+        self.dataTrainList = [
+        '/home/user/workspace/data/20171119-022806/dumbAgent_EnvironmentTypes.AirSim_19-11-2017-02-28-16/',
+        ]
+        self.dataValList = [
+        '/home/user/workspace/data/20171119-031809/dumbAgent_EnvironmentTypes.AirSim_19-11-2017-03-18-20/'
+        #round 1 dagger
+
+
+        ]
