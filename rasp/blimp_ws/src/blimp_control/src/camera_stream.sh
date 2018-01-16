6f88f320497b78df803d2aa5b46fe51274fc6cd6
Ji-Won <park.jiwon@outlook.com>
2018-01-16 02:52:38 +0000

    changed camera start script

--- rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
+++ rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-host_ip=10.0.1.26
+host_ip=10.0.1.37
 port=2224
 
 echo Starting raspivid camera stream
 echo Streaming to $host_ip:$port
 
 # Process is backgrounded with the & operator in Bash
-raspivid -t 0 -w 640 -h 480 -hf -fps 40 -o tcp://10.0.1.26:2224 &
+raspivid -t 0 -w 640 -h 480 -hf -fps 40 -o tcp://$host_ip:$port
 
-echo Running raspivid camera stream at PID $!
+# echo Running raspivid camera stream at PID $!
 
