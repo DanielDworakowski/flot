13de43db1a33c416dfecd02c8f0471ef2c546b7a
Daniel <dworakowskidaniel@gmail.com>
2018-01-27 13:06:51 -0500

    flip the r-pi camera to be the right way up

--- rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
+++ rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-host_ip=10.0.1.37
+host_ip=10.0.1.2
 port=2222
 
 echo Starting raspivid camera stream
 echo Streaming to $host_ip:$port
 
 # Process is backgrounded with the & operator in Bash
-raspivid -t 0 -w 640 -h 480 -hf -fps 40 -o tcp://$host_ip:$port
+raspivid -t 0 -w 640 -h 480 -hf -vf -fps 40 -o tcp://$host_ip:$port
 
 # echo Running raspivid camera stream at PID $!
 
