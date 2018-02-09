b2de82dd4886c023e3966c1c42c6035eb6fdbe32
Daniel Dworakowski <dworakowskidaniel@gmail.com>
2018-02-09 01:31:21 -0500

    fix the navbawq

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ $(function () {
         var secondFeature = $('#features').offset().top;
         var scroll = $(window).scrollTop();
         if (scroll >= 150) {
-            $('.sticky-navigation').css({"background-color": '#d81b60'});
+            $('.sticky-navigation').css({"background-color": '#2196F3'});
         } else {
             $('.sticky-navigation').css({"background-color": 'transparent'});
         }
