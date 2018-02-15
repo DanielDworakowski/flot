477a7ae02c02df8b340db87345b0ac2838b5a2ef
Daniel <dworakowskidaniel@gmail.com>
2018-02-15 00:09:21 -0500

    add requesting the presentation timestamps to the start up

--- rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
+++ rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
@@ -7,7 +7,8 @@ echo Starting raspivid camera stream
 echo Streaming to $host_ip:$port
 
 # Process is backgrounded with the & operator in Bash
-raspivid -t 0 -w 640 -h 480 -hf -vf -fps 40 -o tcp://$host_ip:$port
+name=$(date +%Y%m%d%H%M%S)
+raspivid -t 0 -w 640 -h 480 -hf -vf -fps 40 -o tcp://$host_ip:$port --save-pts $name.csv
 
 # echo Running raspivid camera stream at PID $!
 
