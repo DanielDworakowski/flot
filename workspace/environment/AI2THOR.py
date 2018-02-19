eba9666ede0d1e65d1a1d5e56d539216a95ee563
raejeong <raychanjeong@gmail.com>
2018-02-19 17:43:12 -0500

    take multiple action ai2thor

--- workspace/environment/AI2THOR.py
+++ workspace/environment/AI2THOR.py
@@ -1,14 +1,16 @@
 import ai2thor.controller
 import numpy as np
+import time
 
 class AI2THOR():
     controller = ai2thor.controller.Controller()
-    controller.start()
+    controller.start(player_screen_width=640, player_screen_height=480)
     
-    def __init__(self, scene='FloorPlan28', grid_size=0.001, v_rate=0.1, w_rate=0.1, dt = 1.0):
+    def __init__(self, scene='FloorPlan230', grid_size=0.01, v_rate=0.1, w_rate=0.1, dt = 0.05):
         # Member variables.
         self.controller.reset(scene)
         self.event = self.controller.step(dict(action='Initialize', gridSize=grid_size))
+        self.grid_size = grid_size
         self.position = None
         self.rotation = None
         self.yaw = None
@@ -37,13 +39,13 @@ class AI2THOR():
         self.update()
         return self.image_png
         
-    def getPosition(self):
+    def getRGBImage(self):
         self.update()
         return self.image_rgb
 
     def isCollided(self):
         self.event = self.controller.step(dict(action='MoveAhead'))
-        # self.update()
+        self.update()
         return self.collided
 
     def update(self):
@@ -55,12 +57,42 @@ class AI2THOR():
         self.yaw = self.event.metadata['agent']['rotation']['y']*(np.pi/180.)
         self.collided = self.event.metadata['collided']
 
+    def takeMultipleSteps(self, steps, axis):
+        step_dict = {
+            ('x',True): "MoveRight",
+            ('x', False): "MoveLeft",
+            ('z', True): "MoveAhead",
+            ('z', False): "MoveBack"
+        }
+        is_positive = (steps >= 0)
+
+        success = True
+        for i in range(abs(steps)):
+            self.event = self.controller.step(dict(action=step_dict[(axis,is_positive)]))
+            success = self.event.metadata['lastActionSuccess']
+            time.sleep(0.5)
+        return success
+
+    # def step(self, v_ref, w_ref):
+    #     self.update()
+    #     self.v = (1-self.v_rate)*self.v + self.v_rate*v_ref
+    #     self.w = (1-self.w_rate)*self.w + self.w_rate*w_ref
+    #     new_x = self.position['x'] + self.v*self.dt*np.sin(self.yaw)
+    #     new_z = self.position['z'] + self.v*self.dt*np.cos(self.yaw)
+    #     new_yaw = self.yaw + self.dt*self.w
+    #     self.event = self.controller.step(dict(action='Teleport', x=new_x, y=0.3, z=new_z))
+    #     self.event = self.controller.step(dict(action='Rotate', rotation=new_yaw*(180./np.pi)))
+    #     print(self.event.metadata['agent']['position'])
+
     def step(self, v_ref, w_ref):
-        self.update()
+        # self.update()
         self.v = (1-self.v_rate)*self.v + self.v_rate*v_ref
         self.w = (1-self.w_rate)*self.w + self.w_rate*w_ref
-        new_x = self.position['x'] + self.v*self.dt*np.sin(self.yaw)
-        new_z = self.position['z'] + self.v*self.dt*np.cos(self.yaw)
-        new_yaw = self.yaw + self.dt*self.w
-        self.event = self.controller.step(dict(action='Teleport', x=new_x, y=self.position['y'], z=new_z))
-        self.event = self.controller.step(dict(action='Rotate', rotation=new_yaw*(180./np.pi)))
+        x_steps = int(self.v*self.dt*np.sin(self.yaw)/self.grid_size)
+        z_steps = int(self.v*self.dt*np.cos(self.yaw)/self.grid_size)
+        yaw = self.yaw + self.dt*self.w
+        x_fail = not self.takeMultipleSteps(x_steps, 'x')
+        z_fail = not self.takeMultipleSteps(z_steps, 'z')
+        self.collided = x_fail and z_fail
+        self.controller.step(dict(action='Rotate', rotation=yaw*(180./np.pi)))
+        print(self.collided)
