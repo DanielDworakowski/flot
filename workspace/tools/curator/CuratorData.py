7a9e460360c3c6b801ee96a6f1925e0c8ef64597
Daniel <dworakowskidaniel@gmail.com>
2018-02-23 21:17:55 -0500

    update naming for columns

--- workspace/tools/curator/CuratorData.py
+++ workspace/tools/curator/CuratorData.py
@@ -39,7 +39,7 @@ class CuratorData(object):
         else:
             self.df = pd.read_csv(dataFile)
             self.df['usable'] = 1
-            self.df['labelled'] = 0 
+            self.df['labelled'] = 0
         self.png = self.df['PNG']
         self.size = len(self.png)
         self.df.index.name = idxcol
@@ -52,31 +52,31 @@ class CuratorData(object):
         return self.size
 
     def saveData(self, force = False):
-        # 
-        # Dont save anything if the data was not touched. 
+        #
+        # Dont save anything if the data was not touched.
         if not self.touched and not force:
             print('No change was made to the labels, not saving.')
             return
-        # 
+        #
         # Save the processed / labelled data.
         self.dataIdx += 1
         saveLoc = self.folder + '/processed' + str(self.dataIdx) + '.csv'
         print('saving to ' + saveLoc)
         self.df.to_csv(saveLoc)
-        # 
-        # Reduce the data to only that what was labelled to be usable. 
+        #
+        # Reduce the data to only that what was labelled to be usable.
         saveLoc = self.folder + '/labels.csv'
         labelDf = self.df[self.df['usable'] == 1] # Convert to logical indexing first.
         labelDf.to_csv(saveLoc)
 
     def setUsable(self, flag, startRange, endRange):
-        # 
-        # Ensure that the bounds are respected. 
+        #
+        # Ensure that the bounds are respected.
         if startRange < 0:
             startRange = 0
         if endRange > self.size:
             endRange = self.size - 1
-        # 
+        #
         # Write the data.
         self.df.loc[startRange:endRange, ('usable')] = int(flag)
         self.df.loc[startRange:endRange, ('labelled')] = 1
@@ -88,25 +88,27 @@ class CuratorData(object):
         return np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
 
     def autoLabel(self):
-        arrays = self.__consecutive(np.where(self.df['Sonar:Value'] == 0)[0])
-        mask = np.zeros(len(self.df['Sonar:Value']))
-        masked = ma.masked_array(self.df['Sonar:Value'], mask = mask)
-        self.df['Sonar:Smoothed'] = self.df['Sonar:Value']
-        # 
-        # Assume that if there are multiple zeros in a row this indicates that the 
-        # there is nothing there. Obstacles are beyond the max range of the sensor. 
-        # If there are too few measurements in a row, extend the last measurement. 
+        sonarKey = 'sonar-observation:Value'
+        smoothedKey = 'Sonar:Smoothed'
+        arrays = self.__consecutive(np.where(self.df[sonarKey] == 0)[0])
+        mask = np.zeros(len(self.df[sonarKey]))
+        masked = ma.masked_array(self.df[sonarKey], mask = mask)
+        self.df[smoothedKey] = self.df[sonarKey]
+        #
+        # Assume that if there are multiple zeros in a row this indicates that the
+        # there is nothing there. Obstacles are beyond the max range of the sensor.
+        # If there are too few measurements in a row, extend the last measurement.
         if len(arrays[0]) > 0:
             for array in arrays:
                 masked.mask[array] = 1
                 if len(array) > self.labelConf.consecutiveZeroForPositive:
-                    self.df.loc[array, ('Sonar:Smoothed')] = self.labelConf.maxDistanceMarker
+                    self.df.loc[array, (smoothedKey)] = self.labelConf.maxDistanceMarker
                 else:
-                    self.df.loc[array, ('Sonar:Smoothed')] = self.df['Sonar:Value'][max(array[0] - 1, 0)]
+                    self.df.loc[array, (smoothedKey)] = self.df[sonarKey][max(array[0] - 1, 0)]
             kf = KalmanFilter([1], [1], [0.2**2], [0.2**2])
             means, cov = kf.smooth(masked)
-            # 
+            #
             # Create the labels based on a distance threshold.
-            self.df['Sonar:Smoothed'] = np.squeeze(means)
-        self.df['collision_free'] = self.df['Sonar:Smoothed'] > self.labelConf.distanceThreshold
+            self.df[smoothedKey] = np.squeeze(means)
+        self.df['collision_free'] = self.df[smoothedKey] > self.labelConf.distanceThreshold
         self.df['collision_free'] = self.df['collision_free'].astype(int)
