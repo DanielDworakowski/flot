6e74098dd361ea4fcd689c9176a4fe7a700a2ea5
Ji-Won <park.jiwon@outlook.com>
2018-02-24 16:26:54 +0000

    revert stream script to stream data

--- rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
+++ rasp/blimp_ws/src/blimp_control/src/camera_stream.sh
@@ -8,8 +8,8 @@ echo Streaming to $host_ip:$port
 
 # Process is backgrounded with the & operator in Bash
 name=$(date +%Y%m%d%H%M%S)
-# raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o tcp://$host_ip:$port --save-pts $name.csv
-raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o $name.h264 --save-pts 
+raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o tcp://$host_ip:$port --save-pts $name.csv
+# raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o $name.h264 --save-pts 
 
 # echo Running raspivid camera stream at PID $!
 
