004797b9a81f31258b8012bd3e9f28740112e870
Ji-Won <park.jiwon@outlook.com>
2018-03-08 23:53:21 +0000

    version of launches for daniel`s computer

---
+++ rasp/blimp_ws/src/blimp_control/src/daniel_stream.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+host_ip=10.0.1.102
+port=2222
+
+echo Starting raspivid camera stream
+echo Streaming to $host_ip:$port
+
+# Process is backgrounded with the & operator in Bash
+name=$(date +%Y%m%d%H%M%S)
+raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o tcp://$host_ip:$port
+# raspivid -t 0 -w 640 -h 480 -hf -vf -fps 30 -o $name.h264 --save-pts 
+
+# echo Running raspivid camera stream at PID $!
+
