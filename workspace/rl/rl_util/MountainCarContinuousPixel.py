322b47aefe9d4512ad7225c81f49cbac32b10fc7
raejeong <raychanjeong@gmail.com>
2018-03-13 13:26:37 -0400

    render

--- workspace/rl/rl_util/MountainCarContinuousPixel.py
+++ workspace/rl/rl_util/MountainCarContinuousPixel.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 class Env():
 
     def __init__(self):
-        self.env = gym.make("RoboschoolInvertedPendulum-v1")
+        self.env_name = "RoboschoolInvertedPendulum-v1"
+        self.env = gym.make(self.env_name)
         self.state = self.env.reset()
         self.reward = None
         self.done = False
@@ -15,19 +16,17 @@ class Env():
         self.observation_shape = self.env.observation_space.shape
         # self.observation_shape = self.image.shape
         self.action_shape = self.env.action_space.shape
+        self.env = gym.wrappers.Monitor(self.env, "/home/rae/videos/"+self.env_name)
 
     def step(self, action, render):
         action = np.clip(action,-1,1)
         self.state, self.reward, self.done, _ = self.env.step(action)
-        if render:
-            self.image = self.env.render("rgb_array")
-            plt.imshow(self.image)
-            plt.show()
+        self.image = self.env.render("rgb_array")
         self.image = self.state
         return self.image, self.reward, self.done
 
     def reset(self):
         self.state = self.env.reset()
-        # self.image = self.env.render("rgb_array").copy()
+        self.image = self.env.render("rgb_array")
         self.image = self.state
         return self.image
