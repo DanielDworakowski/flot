c560f909f6cfe36397206bd3ef9f1772a11af69c
Daniel <dworakowskidaniel@gmail.com>
2018-03-19 17:37:41 -0400

    new agents

---
+++ workspace/agents/MultiTrajAgent.py
@@ -0,0 +1,194 @@
+import os
+import torch
+import numpy as np
+from debug import *
+import AgentBase as base
+from Actions import Action
+import Observations as obv
+from torchvision import transforms
+import nn.util.DataUtil as DataUtil
+from torch.autograd import Variable
+from util.visualBackProp import VisualBackProp
+from scipy.misc import imresize
+from PIL import Image
+
+#
+# Neural network agent class.
+
+def combine(orig, filtered, rep_size):
+    if len(rep_size)==2:
+        rep_size.append(3)
+    orignp = np.array(orig)
+    filterednp = filtered.numpy()
+    origsize = orignp.shape
+
+    scaled = imresize(filterednp, rep_size, interp='bicubic')
+
+    delta_w = origsize[1] - rep_size[1]
+    delta_h = origsize[0] - rep_size[0]
+    top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+    left, right = delta_w // 2, delta_w - (delta_w // 2)
+
+    ones = np.ones(scaled.shape)
+    mask= np.logical_not(np.pad(ones, ((top, bottom), (left, right), (0,0)), mode='constant'))
+    padded= np.pad(scaled, ((top, bottom), (left, right), (0,0)), mode='constant')
+    out = np.multiply(mask, orignp) + padded
+
+    return out
+
+class Agent(base.AgentBase):
+    #
+    # Constructor.
+    def __init__(self, conf):
+        super(Agent, self).__init__(conf)
+        self.conf = conf
+        self.toTensor = transforms.ToTensor()
+        #
+        # Check if cuda is available.
+        self.usegpu = torch.cuda.is_available() and self.conf.usegpu
+        self.ToPIL = transforms.ToPILImage()
+        #
+        # If you want to use visualbackprop
+        self.useVisualBackProp = True
+        #
+        # Load the model.
+        if self.conf.modelLoadPath != None and os.path.isfile(self.conf.modelLoadPath):
+            if self.usegpu:
+                checkpoint = torch.load(self.conf.modelLoadPath)
+            else:
+                print('Running the agent on CPU!')
+                checkpoint = torch.load(self.conf.modelLoadPath, map_location={'cuda:0': 'cpu'})
+            self.model = checkpoint['model']
+            if self.useVisualBackProp:
+                self.visualbackprop = VisualBackProp(self.model)
+            self.nnconf = checkpoint['conf']
+            self.model.load_state_dict(checkpoint['state_dict'])
+            printColour('Loaded model from path: %s'%self.conf.modelLoadPath, colours.OKBLUE)
+        else:
+            printError('Could not load model from path: %s'%self.conf.modelLoadPath)
+            raise RuntimeError
+
+        if self.usegpu:
+            self.model.cuda()
+
+        self.model.eval()
+
+        self.model_input_img_shape = conf.image_shape
+        norm = transforms.Normalize([0,0,0], [1,1,1])
+        tnn = self.nnconf.transforms
+        crop = transforms.CenterCrop(self.nnconf.hyperparam.cropShape)
+        for tf in tnn.transforms:
+            if isinstance(tf, DataUtil.Normalize):
+                norm = tf.norm
+                break
+        self.t = transforms.Compose([crop, transforms.ToTensor(), norm])
+        #
+        # Rescale transforms.
+        if any(isinstance(tf, DataUtil.Rescale) for tf in tnn.transforms):
+            self.model_input_img_shape = (self.nnconf.hyperparam.cropShape[0],self.nnconf.hyperparam.cropShape[1],3)
+            self.t = transforms.Compose([
+                transforms.ToPILImage(),
+                crop,
+                transforms.Resize(self.nnconf.hyperparam.image_shape),
+                transforms.ToTensor(),
+                norm,
+            ])
+        else:
+            print('there is no rescale')
+        #
+        # Heuristic Parameters
+        #
+        # action dim for array
+        self.action_array_dim = 11
+        #
+        # minimum probability of collision free to go straight
+        self.straight_min_prob = 0.60
+        #
+        # minimum probability of collision free to stop
+        self.stop_min_prob = 0.0
+        #
+        # min turning probabilty
+        self.turn_min_prob = 0.70
+        #
+        #
+        self.back_min_prob = 0.07
+        # max vt w
+        action_ = Action(np.zeros(self.action_array_dim))
+        self.max_v_t = action_.max_v_t
+        self.max_w = action_.max_w
+    #
+    # Reference to an observation
+    def getActionImpl(self):
+        obs = self.obs
+        npimg = obs['img'].uint8Img
+        # If image is available
+        if npimg is not None:
+            collision_free_prob = []
+            softmax = torch.nn.Softmax()
+            probs = None
+            vbp = None
+
+            img = self.t(npimg)
+
+            if self.usegpu:
+                img = Variable(img.unsqueeze_(0).cuda(async=True))
+            else:
+                img = Variable(img.unsqueeze_(0))
+
+            classActivation = self.model(img)
+            print(classActivation)
+            collision_free_pred = classActivation.data
+            probs = self.model.getClassifications(classActivation, softmax)
+            if self.useVisualBackProp:
+                # tmp = self.ToPIL(self.visualbackprop.visualize(img).squeeze_())
+                tmp = self.visualbackprop.visualize(img)
+                combined = combine(npimg, tmp, self.model_input_img_shape)
+                vbp = Image.fromarray(combined.astype('uint8'), 'RGB')
+
+            collision_free_prob = softmax(classActivation).data.cpu().numpy()
+            print(classActivation)
+            print(collision_free_prob)
+
+            # collision free probability
+            left_prob, center_prob, right_prob = collision_free_prob
+            left_prob, center_prob, right_prob = [left_prob[0], center_prob[0], right_prob[0]]
+            action_array = np.zeros(self.action_array_dim)
+            if center_prob > self.straight_min_prob:
+                #action_array[int(self.action_array_dim/2)] = 1
+                #action = Action(action_array)
+                action = Action(v_t=self.max_v_t,w=0)
+
+            elif left_prob<self.stop_min_prob and center_prob<self.stop_min_prob and right_prob<self.stop_min_prob:
+                action = Action(action_array)
+
+            elif center_prob < self.back_min_prob:
+                action = Action(v_t=-1.25*self.max_v_t,w=0)
+
+            elif left_prob > right_prob and left_prob < self.turn_min_prob:
+                action_array[0] = 1
+                action = Action(action_array)
+
+            elif right_prob >= left_prob and right_prob < self.turn_min_prob:
+                action_array[-1] = 1
+                action = Action(action_array)
+
+            elif left_prob > right_prob:
+                action = Action(v_t=left_prob*self.max_v_t,w=left_prob*self.max_w)
+
+            else:
+                action = Action(v_t=right_prob*self.max_v_t,w=right_prob*self.max_w)
+            #action = Action(action_array)
+            print('_____________________________________________________________________________________________________________________________________')
+            print("Collsion Free Prob: left:{} center:{} right:{}".format(collision_free_prob[0], collision_free_prob[1], collision_free_prob[2]))
+            print("Linear Velocity: {:.2f} Angular Velocity: {:.2f}".format(action.v_t,action.w))
+
+            # Place the activations for visualization.
+            action.meta['activations'] = probs.cpu().numpy()[0]
+            action.meta['visualbackprop'] = vbp
+
+        # Take no action when no image is available
+        else:
+            action = Action(v_t=0,w=0)
+
+        # Do more stuff.
+        return action
--- workspace/nn/config/2TrajConfig.py
+++ workspace/nn/config/2TrajConfig.py
@@ -2,7 +2,6 @@ import torch
 import os
 from debug import *
 import torch.nn as nn
-from models import GenericModel
 from models import MultiTraj_FC
 from models import MultiTraj_conv
 from torchvision import transforms, models
@@ -14,21 +13,21 @@ class Config(DefaultConfig):
     #
     # Initialize.
     def __init__(self, mode = 'train'):
-        nSteps = (0, 0)
-        loadpath = '/disk1/model/rl_normalized-randomshift-lesscolour_model_best.pth.tar'
-        # loadpath = '/disk1/model/rl_newDataFixedLabel_model_best.pth.tar'
+        nSteps = (1, 0)
+        loadpath = '/disk1/model/rl_multiconv-2t-normalized_model_best.pth.tar'
         if mode == 'train':
             loadpath = None
-        super(Config, self).__init__(model = GenericModel.GenericModel(models.resnet18(pretrained=True)), loadPath = loadpath)
+        super(Config, self).__init__(MultiTraj_conv.Resnet_MultiConv(nSteps), loadPath = loadpath)
+        # super(Config, self).__init__(MultiTraj_FC.Resnet_Multifc(nSteps), loadPath = loadpath)
         #
         # The distance threshold for postive / negative.
         self.distThreshold = 0.7
         #
         # How far to shift the image.
-        self.hyperparam.shiftBounds = (30, 0)
+        self.hyperparam.shiftBounds = (110, 0)
         self.hyperparam.nSteps = nSteps
         self.hyperparam.numEpochs = 32
-        self.hyperparam.cropShape = (448, 448)
+        self.hyperparam.cropShape = (400, 400)
         self.epochSaveInterval = 1
         self.modelSavePath = '/disk1/model/'
         #
@@ -39,16 +38,13 @@ class Config(DefaultConfig):
         self.normalize = DataUtil.Normalize(mean, std)
         self.transforms = transforms.Compose([
             Perterbations.RandomShift(self.hyperparam.cropShape, self.hyperparam.shiftBounds, self.hyperparam.nSteps, mode),
-            # Perterbations.CenterCrop(self.hyperparam.cropShape),
-            DataUtil.Rescale(self.hyperparam.image_shape),
-            # Perterbations.CenterCrop(self.hyperparam.image_shape),
             Perterbations.RandomHorizontalFlip(0.5, mode),
+            DataUtil.Rescale(self.hyperparam.image_shape),
             Perterbations.ColourJitter(0.05, 0.05, 0.05, 0.05, mode), # The effects of this must be tuned.
             DataUtil.ToTensor(),
             self.normalize,
         ])
-        self.experimentName = 'rl_normalized-newData'
-        # self.dataValList = ['/disk1/rldata/20180306_012910',]
+        self.experimentName = 'rl_multiconv-2t-normalized'
         self.dataValList = [
             '/disk1/rldata/20180304_042341',
             '/disk1/rldata/20180306_012910',
@@ -60,5 +56,4 @@ class Config(DefaultConfig):
             '/disk1/rldata/20180314_155329',
             '/disk1/rldata/20180314_172112',
             '/disk1/rldata/20180315_020652',
-            '/disk1/rldata/20180317_030020',
         ]
